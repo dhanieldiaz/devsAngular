e2dc6c5a02e2a8c93a8cad978eeada27dc46c75a
Dhaniel Diaz <dhanieldiaz@gmail.com>
2018-10-20 18:44:21 -0500

    Firt commit, modules and core

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ContactComponent } from './modules/contact/contact.component';
+import { AboutUsComponent } from './modules/about-us/about-us.component';
+import { OurWorkComponent } from './modules/our-work/our-work.component';
+import { HomeComponent } from './modules/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path: '', pathMatch: 'full', component: HomeComponent},
+	{path: 'home', component: HomeComponent},
+	{path: 'contact', component: ContactComponent},
+	{path: 'about-us', component: AboutUsComponent},
+	{path: 'our-work', component: OurWorkComponent},
+	{path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [
+		RouterModule.forRoot(routes, {useHash: true})
+	],
+	exports: [
+		RouterModule
+	]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './modules/contact/contact.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { AboutUsComponent } from './modules/about-us/about-us.component';
+import { OurWorkComponent } from './modules/our-work/our-work.component';
+import { HomeComponent } from './modules/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		ContactComponent,
+		HeaderComponent,
+		FooterComponent,
+		AboutUsComponent,
+		OurWorkComponent,
+		HomeComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
